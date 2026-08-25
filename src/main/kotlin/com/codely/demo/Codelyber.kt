e07d1b24558e2353ca3edae2c07ea7b75b25c9a2
snayder15 <jhumpiri1599@gmail.com>
2026-08-25 19:12:54 -0300

    Primeros pasos en el manejo de valores nulos

--- src/main/kotlin/com/codely/demo/Codelyber.kt
+++ src/main/kotlin/com/codely/demo/Codelyber.kt
@@ -2,6 +2,13 @@ import java.time.LocalDate
 
 fun main() {
     println("Please enter a date with the format <yyyy-MM-dd>")
-    val input = LocalDate.parse(readln())
-    println("Your wrote $input ${input.dayOfWeek}")
+    val readLine: String? = null
+//    val input = LocalDate.parse(readLine) //TODO: Rompe en el metodo LocalDate.parse
+//    val input = LocalDate.parse(readLine ?: "2026-09-26") //TODO: Usamos el operador elvis operator para devolver un valor por defecto, cuando readline es null
+//    val input = LocalDate.parse(readLine!!) //TODO: Rompe en el metodo main
+    readLine?.let{
+        val input = LocalDate.parse(readLine)
+        println("Your wrote $input ${input.dayOfWeek}")
+    }
+//    println("Your wrote $input ${input.dayOfWeek}")
 }
