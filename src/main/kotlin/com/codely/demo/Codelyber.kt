b14a3f92bbaacbce39500f8f304031b8e0f362af
snayder15 <jhumpiri1599@gmail.com>
2026-08-30 02:05:25 -0300

    Parte 2, Aplicando scope functions de kotlin.

--- src/main/kotlin/com/codely/demo/Codelyber.kt
+++ src/main/kotlin/com/codely/demo/Codelyber.kt
@@ -2,6 +2,7 @@ package com.codely.demo
 
 import java.time.LocalDate
 import java.time.Period
+import kotlin.system.exitProcess
 
 class Codelyber {
     fun greet() = "Welcome to kotlin skeleton!!!"
@@ -9,13 +10,22 @@ class Codelyber {
 
 fun main() {
     println("Please enter your birthday with the format <yyyy-MM-dd>")
-    supportNullableString(readLine()).takeUnless {
-        it.isNullOrEmpty() || it.isNullOrBlank();
-    }?.let { line ->
-        val input = LocalDate.parse(line).also {
-            println("You wrote $it")
+//    supportNullableString(readLine()).takeUnless { // -> si es verdadero devuelve null si no devuelve line
+//        it.isNullOrEmpty() || it.isNullOrBlank();
+//    }
+    supportNullableString(readLine()).takeIf { // -> Si es verdadero devuelve line si no devuelve null
+        !it.isNullOrEmpty() && !it.isNullOrBlank();
+    }?.let {
+        LocalDate.parse(it)
+    }.apply {
+        if (this == null) {
+            println("The date is not valid")
+            exitProcess(1)
         }
-        with(Period.between(input, LocalDate.now())) {
+    }?.also {
+        println("You wrote $it")
+    }.run {
+        with(Period.between(this, LocalDate.now())) {
             println("The difference between the date you wrote and today is ${this.years}")
         }
     }
