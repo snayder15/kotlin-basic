a0e6508ee05178d6dc53926139a833da2e91aa9a
snayder15 <jhumpiri1599@gmail.com>
2026-08-29 21:41:36 -0300

    Aplicando scope functions de kotlin

--- src/main/kotlin/com/codely/demo/Codelyber.kt
+++ src/main/kotlin/com/codely/demo/Codelyber.kt
@@ -2,7 +2,6 @@ package com.codely.demo
 
 import java.time.LocalDate
 import java.time.Period
-import kotlin.system.exitProcess
 
 class Codelyber {
     fun greet() = "Welcome to kotlin skeleton!!!"
@@ -10,16 +9,16 @@ class Codelyber {
 
 fun main() {
     println("Please enter your birthday with the format <yyyy-MM-dd>")
-    val line = supportNullableString(readLine())
-    if (line.isNullOrEmpty()) {
-        println("data is not valid")
-        exitProcess(1)
+    supportNullableString(readLine()).takeUnless {
+        it.isNullOrEmpty() || it.isNullOrBlank();
+    }?.let { line ->
+        val input = LocalDate.parse(line).also {
+            println("You wrote $it")
+        }
+        with(Period.between(input, LocalDate.now())) {
+            println("The difference between the date you wrote and today is ${this.years}")
+        }
     }
-    val input = LocalDate.parse(line)
-    println("You wrote $input")
-    val currentDate = LocalDate.now()
-    val difference = Period.between(input, currentDate)
-    println("The difference between the date you wrote and today is ${difference.years}")
 }
 
 fun supportNullableString(line: String?) = line
